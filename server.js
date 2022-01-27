2ed7cd6fcfa1d84244abfb20bc387c4bff9483dc
Qisas Hasanudin <qisas.hasanudin@gmail.com>
2022-01-28 00:32:06 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
