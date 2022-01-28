1fe58e234c1e8477841c57a7b3326137316ecd0e
Qisas Hasanudin <qisas.hasanudin@gmail.com>
2022-01-28 09:02:12 +0700

    day 1

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+
+connectDB();
 
 const app = express();
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
 });
